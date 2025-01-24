07e056a7a7146b7dc902cc9f301bc521d3775519
Krishan019 <86590126+Krishan019@users.noreply.github.com>
2025-01-24 11:44:27 +0530

    Multiple line comments added.

--- arrays/second_largest.py
+++ arrays/second_largest.py
@@ -1,6 +1,6 @@
-#Problem Statement
-# Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
-# Note: The second largest element should not be equal to the largest element.
+'''Problem Statement:
+Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+Note: The second largest element should not be equal to the largest element.'''
 
 
 # Approach 1
